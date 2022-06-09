5fd3a015e3126aa16e3b9c059b7f4306edff9444
sipher69 <jilanisaif2@gmail.com>
2022-06-09 09:44:32 +0300

    09/06/2022(2)

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const fetchArticles = async () => {
 
       try {
-        const res = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${term}&api-key=${process.env.REACT_APP_ARTICLES_API_KEY}`)
+        const res = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${term}&api-key=RXQrHBOX22Ueq3N8AAF7YW8ZAUhi20mK`)
         const articles = await res.json();
         console.log(articles.response.docs);
         setArticles(articles.response.docs);
